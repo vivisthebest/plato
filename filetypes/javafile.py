f7a51defe9ff77de530611e6b76734b7298173f2
asafer <asafer@bu.edu>
2015-04-02 20:52:58 -0400

    finished javareviewtest

--- filetypes/javafile.py
+++ filetypes/javafile.py
@@ -30,78 +30,9 @@ def _print_file(path):
 
 
 
-class ReviewTest(BaseTest):
-    yaml_type = 'review'
-
+class JavaReviewTest(ReviewTest):
     def __init__(self, dict_, file_type):
         super().__init__(dict_, file_type)
-
-        if type(dict_['deduction']) is dict:
-            mode, deductions = dict_['deduction'].popitem()
-            if mode not in DEDUCTION_MODE_TYPES:
-                raise ValueError("invalid deduction mode '{}' in criteria; "
-                                 "should be one of {}".format(mode,
-                                 DEDUCTION_MODE_TYPES))
-
-            self.deduction = []
-            for d in deductions:
-                value, reason = d.popitem()
-                value = int(value)
-
-                self.deduction.append((value, reason))
-
-            self.deduction_mode = mode
-
-        else:
-            self.deduction = dict_['deduction']
-
-
-    def __str__(self):
-        return "review of {} ({} pts.)".format(self.target,
-                                               self.deduction)
-
-
-    def run(self, path, print_file=True):
-        from functools import reduce
-        import prompt
-
-        if print_file:
-            _print_file(path)
-
-        sprint("description: " + self.description)
-
-        if type(self.deduction) is int:
-            choices = ["do not take this deduction",
-                       "take this deduction (-{} points)".format(
-                           self.deduction)]
-            got = prompt.prompt(choices, '1')
-
-            if got == [1]:
-                return {'deduction': self.deduction,
-                        'description': self.description}
-            else:
-                sprint("taking no points")
-                return None
-
-        elif type(self.deduction) is list:
-            choices = ["{} (-{} {})".format(y, x, plural("point", x)) for
-                       x, y in self.deduction]
-            got = prompt.prompt(choices, self.deduction_mode)
-
-            if sum(map(lambda x: self.deduction[x][0], got)) > 0:
-                deductions = []
-
-                for s in got:
-                    if self.deduction[s][0] > 0:
-                        d = {'deduction': self.deduction[s][0],
-                             'description': self.deduction[s][1]}
-                        deductions.append(d)
-
-                return deductions
-            else:
-                sprint("taking no points")
-                return None
-
 
 
 class DiffTest(BaseTest):
