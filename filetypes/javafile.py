8389b016473f34969dc0d76cc6089314cc2beb80
asafer <asafer@bu.edu>
2015-04-02 21:15:45 -0400

    Update javafile.py

--- filetypes/javafile.py
+++ filetypes/javafile.py
@@ -10,66 +10,17 @@ import yaml
 
 DEDUCTION_MODE_TYPES = prompt.PROMPT_MODES
 
-def _print_file(path):
-    f = open(path, 'rb')
 
-    while True:
-        c = f.read(1)
-        if not c:
-            break
-        if c == '\r':
-            continue
-        if c == '\n':
-            print()
-            continue
-
-        try:
-            print(c.decode('utf-8'), end='')
-        except UnicodeDecodeError:
-            print(COLOR_INVERTED + '?' + COLOR_RESET, end='')
-
-
-
-class JavaReviewTest(ReviewTest):
-    def __init__(self, dict_, file_type):
-        super().__init__(dict_, file_type)
-
-
-class DiffTest(BaseTest):
-    yaml_type = 'diff'
-
-    def __init__(self, dict_, file_type):
-        import os
-
-        super().__init__(dict_, file_type)
-
-        if not os.path.isfile(config.static_dir + os.sep + dict_['against']):
-            raise ValueError("solution file for diff ('{}') "
-                             "cannot be found".format(dict_['against']))
-
-        self.against = config.static_dir + os.sep + dict_['against']
-
-
-    def run(self, submission):
-        import filecmp
-
-        if filecmp.cmp(submission, self.against):
-            return None
-        else:
-            return {'deduction': self.deduction,
-                    'description': self.description,
-                    'notes': ["files do not match"]}
-
-
-class JavaFile(BaseFile):
+class JavaFile(PlainFile):
     yaml_type = 'java'
     extensions = ['java']
-
+    supported_tests = PlainFile.supported_tests.copy()
+    supported_tests.append()
 
 class PlainFile(BaseFile):
     yaml_type = 'plain'
     extensions = ['txt']
-    supported_tests = BaseFile.supported_tests.copy()
+    
     supported_tests.append(DiffTest)
     supported_tests.append(ReviewTest)
 
